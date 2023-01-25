a26cbcf2f684656c1ff414c7c043af822948a52a
Paul Yeo <paulyeo@amazon.com>
2023-01-25 21:53:28 +0000

    Calculate probability of collision

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,9 @@
-use std::collections::HashSet;
-use std::sync::{Arc, Mutex};
-use std::thread;
-
-use uuid::Uuid;
-
-fn find_me_a_collision(mutex: Arc<Mutex<Vec<u64>>>) {
-    let mut partitions = HashSet::new();
-    let mut i = 0;
-
-    loop {
-        // if i == 1000000000 {
-        //     let mut guard = mutex.lock().unwrap();
-        //     guard.push(i);
-        //     break;
-        // }
-
-        let (_, partition_id) = Uuid::new_v4().as_u64_pair();
-
-        if partitions.contains(&partition_id) {
-            let mut guard = mutex.lock().unwrap();
-            guard.push(i);
-            break;
-        }
-
-        partitions.insert(partition_id);
-        i = i + 1;
-    }
-}
+use std::f64::consts::E;
 
 fn main() {
-    let mutex: Arc<Mutex<Vec<u64>>> = Arc::new(Mutex::new(Vec::new()));
+    let n = 2.0_f64.powf(58.0);
+    let k = 100000.0_f64;
 
-    thread::scope(|s| {
-        for _ in 0..10 {
-            s.spawn(|| {
-                let mutex = mutex.clone();
-                find_me_a_collision(mutex);
-            });
-        }
-    });
-
-    println!("{:#?}", mutex.lock().unwrap());
+    let res = 1.0 - E.powf((-k * (k - 1.0)) / (2.0 * n));
+    println!("{}", res);
 }
